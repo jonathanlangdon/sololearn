89ff33b5325708ff2143d10a1fdebab78ca4f367
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-06-07 23:15:54 -0400

    declaring variables

---
+++ Intro to Java/02 Declaring variables.java
@@ -0,0 +1,8 @@
+public class Program {
+    public static void main(String[] args) {
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        System.out.println(alphabet);
+    }
+}
+
+//example of delaring a variable
