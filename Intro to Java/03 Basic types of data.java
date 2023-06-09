f970bcdbd37954b97252f490521c2d70c0224fd6
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-06-08 22:13:23 -0400

    Basic types of data

---
+++ Intro to Java/03 Basic types of data.java
@@ -0,0 +1,11 @@
+public class Program {
+    public static void main(String[] args) {
+        String label = "Balance";
+        Double balance = 599.84;
+
+        System.out.println(label);
+        System.out.println(balance);
+    }
+}
+
+//also, float and int and Boolean
