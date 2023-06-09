340f151591f81741a5a02f01beeee9b0e2c06b3c
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-06-08 22:15:07 -0400

    boiling water or not

---
+++ Intro to Java/07 Boiling or not.java
@@ -0,0 +1,13 @@
+import  java.util.Scanner;
+
+public class Program {
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    int temp = scn.nextInt();
+    if (temp >= 100) {
+      System.out.println("Boiling");
+    } else {
+      System.out.println("Not boiling");
+    }
+  }
+}
