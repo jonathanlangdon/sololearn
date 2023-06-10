73709a9c58b24504044176cf5882cbdd6125eb9f
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-06-10 16:27:42 -0400

    making new methods

---
+++ Intro to Java/15 making new methods.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Program {
+  static void bot() {
+      Scanner sc = new Scanner(System.in);
+      int num = sc.nextInt();
+      if (num == 1) {
+        System.out.println("Order confirmed");
+      } else if (num == 2) {
+         System.out.println("info@sololearn.com");
+    } else {
+      System.out.println("Try again");
+    }
+  } 
+  public static void main(String[] args) {
+    bot();
+  }
+ 
+}
