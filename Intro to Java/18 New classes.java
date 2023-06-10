928afea7a46772af08a670cbc9557c24f4cc8503
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-06-10 16:28:18 -0400

    new classes

---
+++ Intro to Java/18 New classes.java
@@ -0,0 +1,12 @@
+public class Main {
+  public static void main(String[] args) {
+  Loading loading = new Loading();
+  loading.LoadingMessage();
+  }
+}
+
+class Loading {
+  public static void LoadingMessage(){
+    System.out.print("Loading");
+  }
+}
