308cb0ce58a35187ebe3b49e24f588c01df9a08c
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-06-11 18:06:57 -0400

    encapsulation

---
+++ Intro to Java/24 Encapsulation.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main
+{
+public static void main(String[] args) {
+    Scanner read = new Scanner(System.in);
+    int a = read.nextInt();
+   
+    Pupil pupil =  new Pupil();
+            pupil.setAge(a);
+}
+}
+
+class Pupil{
+    private int age;
+   
+    //complete setter method
+    public void  setAge(int a){
+       System.out.println(a > 6 ? "Welcome" : "Sorry");
+    }
+   
+    public int getAge(){
+        return age;
+    }
+}
