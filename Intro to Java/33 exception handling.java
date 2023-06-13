d372964cbae50ddfebceeb2738ac8d810cd77a33
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-06-13 15:05:18 -0400

    exception handling

---
+++ Intro to Java/33 exception handling.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main
+{
+public static void main(String[] args) {
+   
+    Scanner scanner = new Scanner(System.in);
+    int choice = scanner.nextInt();
+   
+    String[] categories = {"PCs", "Notebooks", "Tablets", "Phones", "Аccessories"};
+   
+    try {
+        System.out.println(categories[choice]);
+    } catch(Exception e) {
+        System.out.println("Wrong Option");
+    }
+}
+}
