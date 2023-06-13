ee4803361e16a4cac734ba921d50fc141b98e5a6
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-06-13 15:07:24 -0400

    linked lists

---
+++ Intro to Java/37 linked lists.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[ ] args) {
+     Scanner scanner = new Scanner(System.in);
+        
+        ArrayList<Integer> evennums = new ArrayList<Integer>();
+        int sum = 0;
+        while(evennums.size()<3){
+            int num = scanner.nextInt();
+            //your code goes here
+            evennums.add(num);
+            sum += num;
+        }
+        
+        //calculate and output the average integer value
+        System.out.println(sum/3);
+    }
+}
