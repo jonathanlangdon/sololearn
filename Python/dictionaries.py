d5f29b5d51cb19d3e776324859eb185e30ee506e
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-06-19 23:20:16 -0400

    commenting dictionaries

--- Python/dictionaries.py
+++ Python/dictionaries.py
@@ -1,9 +1,5 @@
-car = {
-    'brand':'BMW',
-    'year': 2018,
-    'color': 'red',
-    'mileage': 15000
-}
+# dictionaries have a key (first) and values (second)
+car = {"brand": "BMW", "year": 2018, "color": "red", "mileage": 15000}
 
 a = input()
 print(car[a])
