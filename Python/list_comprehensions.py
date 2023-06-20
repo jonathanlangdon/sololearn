9be14784a825fdfe7d8a510f4fa14dd1501bbb45
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-06-19 23:21:10 -0400

    list comprehensions commenting

--- Python/list_comprehensions.py
+++ Python/list_comprehensions.py
@@ -1,4 +1,5 @@
 word = input()
-vowels = {'a','e','i','o','u'}
+vowels = {"a", "e", "i", "o", "u"}
+# list comprehensions use the following format to loop through the list
 word_consts = [c for c in word if c not in vowels]
 print(word_consts)
