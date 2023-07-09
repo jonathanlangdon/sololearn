cd204112bf7e222cf449a47a5b7be75237d2a1a2
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-07-08 21:10:18 -0400

    // Inheritance

---
+++ Python/Inheritance.py
@@ -0,0 +1,22 @@
+// Inheritance
+
+class Shape: 
+    def __init__(self, w, h):
+        self.width = w
+        self.height = h
+
+    def area(self):
+        print(self.width*self.height)
+
+class Rectangle(Shape):
+    #your code goes here
+    def perimeter(self):
+        print(2 * (self.width + self.height))
+    
+
+w = int(input())
+h = int(input())
+
+r = Rectangle(w, h)
+r.area()
+r.perimeter()
