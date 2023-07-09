5d16d85a8e84b4db9898e0ac3cec6b6b57c6ab2e
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-07-08 21:10:21 -0400

    #Preservation and private methods

---
+++ Python/Privacy.py
@@ -0,0 +1,17 @@
+#Preservation and private methods
+
+class Player:
+    def __init__(self, name, lives):
+        self.name = name
+        self._lives = lives
+
+    def hit(self):
+        #your code goes here
+        self._lives -= 1
+        if self._lives == 0:
+            print("Game Over")
+
+p = Player("Cyberpunk77", 3)
+p.hit()
+p.hit()
+p.hit()
