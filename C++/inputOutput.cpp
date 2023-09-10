370f330e1eb07e3ccbff52ecf0c9ffd27c964d41
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-09-10 18:08:40 -0400

    // practice using cin and cout for console logging and getting user input

---
+++ C++/inputOutput.cpp
@@ -0,0 +1,11 @@
+// practice using cin and cout for console logging and getting user input
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    double length, height;
+
+    cin >> length >> height;
+    cout << length * height;
+}
