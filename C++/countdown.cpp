9c2c05c337a567ad93250bb67537d2ea8a1950ec
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-09-10 18:08:29 -0400

    // practice with a while loop for a countdown

---
+++ C++/countdown.cpp
@@ -0,0 +1,15 @@
+// practice with a while loop for a countdown
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+    cin >> number;
+    while (number >= 0) {
+        cout << number << endl;
+        number--;
+    }
+    
+
+}
