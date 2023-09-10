3db8c0a2b8b99138decb7799a7670f85e46a6870
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-09-10 18:08:33 -0400

    // a basic do while loop

---
+++ C++/doWhileExample.cpp
@@ -0,0 +1,12 @@
+// a basic do while loop
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num = 0;
+    do {
+        cout << num << endl;
+        num+=2;
+    } while(num <= 10);
+}
