cf832e1c83cd70d9fc7e0f6767a3ccbc99b7e4ab
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-09-10 18:08:37 -0400

    // practice with if else statements

---
+++ C++/ifElseStatements.cpp
@@ -0,0 +1,14 @@
+// practice with if else statements
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age;
+    cin >> age;
+    if (age < 16) {
+        cout << "Not allowed";
+    } else {
+        cout << "Welcome";
+    }
+}
