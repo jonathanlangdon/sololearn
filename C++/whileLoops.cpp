05c468323bd354fe95cf12b01cb27240524e7b19
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-09-11 19:19:46 -0400

    // a basic while loop

---
+++ C++/whileLoops.cpp
@@ -0,0 +1,12 @@
+// a basic while loop
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num = 5;
+    while(num > 0) {
+        cout << num << endl;
+        num -= 1;
+    }
+}
