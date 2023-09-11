55674a567f99b88caf80eab958301af0bafce503
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-09-11 19:19:30 -0400

    // using an array, user input selects item from array

---
+++ C++/arrayAccess.cpp
@@ -0,0 +1,12 @@
+// using an array, user input selects item from array
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    char letters[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+    int num;
+    cin >> num;
+    cout << letters[num];
+}
