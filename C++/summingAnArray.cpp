6eaf4e75c09219a23e23702bfcfcb305b08aa8d1
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-09-11 19:19:43 -0400

    // using a for-each loop to sum up contents of an array

---
+++ C++/summingAnArray.cpp
@@ -0,0 +1,12 @@
+// using a for-each loop to sum up contents of an array
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    char letters[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+    int num;
+    cin >> num;
+    cout << letters[num];
+}
