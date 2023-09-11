a63597a03ea5f8339d81e24cebb359a8cc07e85c
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-09-11 19:20:11 -0400

    // printing to the console

---
+++ C#/fixedPrintStatement.cs
@@ -0,0 +1,11 @@
+// printing to the console
+
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("My first C# Code Coach!");
+    }
+}
