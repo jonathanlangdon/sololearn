b723573d0c493d34ee0b4699b033ef26353d19b8
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-09-13 21:43:42 -0400

    // create a variable length array from input

---
+++ C++/arrayFromUserInput.cpp
@@ -0,0 +1,23 @@
+// create a variable length array from input
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n; //size of the array
+    //your code goes here
+    int *nums = new int[n];
+    
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    int max = nums[0];
+    for(int i=0; i<n; i++) {
+        if(nums[i]>max)
+            max = nums[i];
+    }
+    cout << max;
+
+}
