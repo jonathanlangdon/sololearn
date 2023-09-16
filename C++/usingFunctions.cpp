b1dce071da8763e83551caf887635d8704cd8246
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-09-16 19:06:46 -0400

    // create a function that doesn't return anything

---
+++ C++/usingFunctions.cpp
@@ -0,0 +1,14 @@
+// create a function that doesn't return anything
+
+#include <iostream>
+using namespace std;
+
+//define the loading() function
+void loading() {
+    cout << "Loading...";
+}
+
+int main() {
+    //call the funnction
+    loading();
+}
