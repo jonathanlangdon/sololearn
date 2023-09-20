9280e277aa3bfbc97d5f7044b055c6d96e6ce01d
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-09-19 22:06:03 -0400

    -- Retrieve the names of employees who have been with the company for more

---
+++ SQL/employees_over_five_years.sql
@@ -0,0 +1,5 @@
+-- Retrieve the names of employees who have been with the company for more 
+than 5 years from the "Employees" table. The "Employees" table should have 
+at least two columns: "Name" and "YearsWithCompany" 
+
+SELECT Name from Employees WHERE YearsWithCompany > 5
