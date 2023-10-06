7a9ea1699baca3eaf646b3df795097e7a2311068
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-10-06 14:29:24 -0400

    // Using constructors

---
+++ C#/constructors.cs
@@ -0,0 +1,27 @@
+// Using constructors
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Project project = new Project();
+        }
+    }
+    class Project
+    {
+        //create a constructor to show "Project created"
+        public Project() {
+        
+        Console.WriteLine("Project created");
+        }
+    }
+}
