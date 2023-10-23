147a8e38ba2144cfa2bb7bb1b65cdd570578fa8a
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-10-22 21:50:58 -0400

    // practice using System namespace

---
+++ C#/usingSystemNameSpace.cs
@@ -0,0 +1,12 @@
+// practice using System namespace
+
+using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("C# is awesome!");
+        Console.WriteLine("Coding is fun");
+    }
+}
