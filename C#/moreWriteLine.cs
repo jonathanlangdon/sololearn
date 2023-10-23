210a452d91f97d867f1859a9f7a875948a543ec9
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-10-22 21:50:52 -0400

    // more practice with WriteLine

---
+++ C#/moreWriteLine.cs
@@ -0,0 +1,12 @@
+// more practice with WriteLine
+
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("Welcome!");
+        Console.WriteLine("Enter your address.");
+    }
+}
