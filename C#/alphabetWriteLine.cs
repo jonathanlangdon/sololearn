72d2b21388afd3fadbec2a29069785316475905d
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-10-22 21:50:46 -0400

    // practice with WriteLine

---
+++ C#/alphabetWriteLine.cs
@@ -0,0 +1,12 @@
+// practice with WriteLine
+
+using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string alphabet = "abcdefghijklmnopqrstuvwxyz";
+        Console.WriteLine(alphabet);
+    }
+}
