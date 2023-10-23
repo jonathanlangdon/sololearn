0192cb88692db34d1a80a2bddce00e2ebe6d38c1
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-10-22 21:50:55 -0400

    // WriteLine a string and an int

---
+++ C#/thatsThePrice.cs
@@ -0,0 +1,12 @@
+// WriteLine a string and an int
+
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Price:");
+        Console.WriteLine(499);
+    }
+}
