c35131259cd04143313ecdf49a35c5e2a51d971b
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-10-22 21:50:49 -0400

    // practice with comments

---
+++ C#/codingForWho.cs
@@ -0,0 +1,12 @@
+// practice with comments
+
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Console.WriteLine("Coding is for engineers");
+        Console.WriteLine("Coding is for everyone");
+    }
+}
