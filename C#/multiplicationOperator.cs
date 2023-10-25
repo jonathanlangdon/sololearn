7cfb9bff786ba997ea57678267ca512ad45971c4
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-10-25 19:11:52 -0400

    // practice with math operator - multiplication

---
+++ C#/multiplicationOperator.cs
@@ -0,0 +1,12 @@
+// practice with math operator - multiplication
+
+using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(30 * 24 * 60 * 60);
+        
+    }
+}
