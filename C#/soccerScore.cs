e06cf23d4f0fa80c585717009d313a06729430ef
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-10-25 19:11:58 -0400

    // Making a soccer score - practice with math operators

---
+++ C#/soccerScore.cs
@@ -0,0 +1,15 @@
+// Making a soccer score - practice with math operators
+
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int wins = 5;
+        int draws = 2;
+
+        Console.WriteLine(wins * 3 + draws);
+        
+    }
+}
