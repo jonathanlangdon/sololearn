c6843141f8fb2806668826f255cee90c2bb591be
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-10-25 19:11:55 -0400

    // school system data - practice with types

---
+++ C#/schoolSystem.cs
@@ -0,0 +1,19 @@
+// school system data - practice with types
+
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string name = "John";
+        int age = 21;
+        double score = 4.3;
+        bool isGraduated = false;
+
+        Console.WriteLine(name);
+        Console.WriteLine(age);
+        Console.WriteLine(score);
+        Console.WriteLine(isGraduated);
+    }
+}
