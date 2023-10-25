0b39983ef626cd7738dc43f667abb758624a6978
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-10-25 19:12:01 -0400

    // subtraction assignment operator practice

---
+++ C#/subtractionAssignment.cs
@@ -0,0 +1,13 @@
+// subtraction assignment operator practice
+
+using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int balance = 416500;
+        int withdraw = 160000;
+        Console.WriteLine(balance -= withdraw);        
+    }
+}
