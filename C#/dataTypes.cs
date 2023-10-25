16868b6e0c5c50be59b7a2b98d17ca2d0b75fc37
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-10-25 19:11:49 -0400

    // practice with 2 of the data types in C#

---
+++ C#/dataTypes.cs
@@ -0,0 +1,16 @@
+// practice with 2 of the data types in C#
+
+sing System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        
+        string name = "Tom";
+        int score = 163;
+
+        Console.WriteLine(name);
+        Console.WriteLine(score);
+    }
+}
