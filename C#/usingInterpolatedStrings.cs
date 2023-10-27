590e80d5af9376022dc49287beb2aa3deace16b9
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-10-26 22:08:47 -0400

    // practice using interpolated strings

---
+++ C#/usingInterpolatedStrings.cs
@@ -0,0 +1,16 @@
+// practice using interpolated strings
+
+using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string country = Console.ReadLine();
+        string capital = Console.ReadLine();
+
+        Console.WriteLine($"Country: {country}, Capital: {capital}");
+
+       
+    }
+}
