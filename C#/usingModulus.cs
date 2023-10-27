481914085c3eaa0e6005e7557ba1f1c7bbe1e390
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-10-26 22:08:50 -0400

    // practice with modulus

---
+++ C#/usingModulus.cs
@@ -0,0 +1,15 @@
+// practice with modulus
+
+using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int items = 200;
+        int itemInBox = 9;
+        Console.WriteLine(items % itemInBox);
+
+       
+    }
+}
