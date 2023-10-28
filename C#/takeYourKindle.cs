e547f5e956dc30876b61ac4ea78ad19f504540a9
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-10-28 18:52:35 -0400

    // practice with if statements

---
+++ C#/takeYourKindle.cs
@@ -0,0 +1,18 @@
+// practice with if statements
+
+using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        //your code goes here
+        int maxAge = 19;
+        if (age <= maxAge)
+        {
+            Console.WriteLine("Take your kindle");
+        }
+    }
+}
