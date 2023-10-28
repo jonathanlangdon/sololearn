720e764c4a3e81c02968bfa9bbea137b974094fc
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-10-28 16:29:57 -0400

    // practice writing if statements

---
+++ C#/lightsOn.cs
@@ -0,0 +1,17 @@
+// practice writing if statements
+
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int hour = Convert.ToInt32(Console.ReadLine());
+        
+        if (hour >= 19)
+        {
+            Console.WriteLine("Lights On");
+        }
+        
+    }
+}
