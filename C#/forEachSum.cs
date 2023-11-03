f3dae59c5237b6a2ca39c423dd5f3af8e24f1891
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-11-03 14:28:43 -0400

    // use a forEach loop for making a sum if it's even in an array

---
+++ C#/forEachSum.cs
@@ -0,0 +1,35 @@
+// use a forEach loop for making a sum if it's even in an array
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[5];
+            int count = 0;
+            while (count <5)
+            {
+                numbers[count] = Convert.ToInt32(Console.ReadLine());
+                count++;
+            }
+            int sum = 0;
+            foreach(int num in numbers)
+            {
+                if (num % 2 == 0)
+                {
+                    sum += num;
+                }
+            }
+            Console.WriteLine(sum);
+            
+        }
+    }
+}
