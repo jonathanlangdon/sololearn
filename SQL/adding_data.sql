96a29c680db95e6373ebadd921c1b3c3bae10e86
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-12-04 13:47:33 -0500

    -- insert new values into the table, then query select

---
+++ SQL/adding_data.sql
@@ -0,0 +1,10 @@
+-- insert new values into the table, then query select
+
+INSERT INTO Employees (firstname, lastname, salary) 
+VALUES
+    ('Wang', 'Lee', 1900),
+    ('Greta', 'Wu', 1200);
+
+
+SELECT id, firstname, lastname, salary FROM Employees
+ORDER BY id DESC
