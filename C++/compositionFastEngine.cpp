89c52f5fd760e903251f2e5f01d055e7111259de
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-12-06 16:26:24 -0500

    // using composition and classes to call functions of classes

---
+++ C++/compositionFastEngine.cpp
@@ -0,0 +1,40 @@
+// using composition and classes to call functions of classes
+
+#include <iostream>
+using namespace std;
+
+class Engine {
+    public:
+        Engine(int p): power(p) {};
+        void start() {
+            cout <<"Engine ON ("<<power<<" horsepower)";
+        }
+    private:
+        int power;
+};
+class Car {
+    public:
+        Car(Engine x, string c, int y): e(x), color(c), year(y) {};
+        void start() {
+            cout <<"Starting"<<endl;
+            //your code goes here
+            e.start();
+           
+        }
+    private:
+        Engine e;
+        string color;
+        int year;
+};
+
+int main() {
+    int power;
+    string color;
+    int year;
+    cin >> power >> color >> year;
+   
+    Engine e(power);
+    //your code goes here
+    Car c(e, color, year);
+    c.start();
+}
