6867a3d524129e44495a0b35cf1beb82a1f66adf
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-12-13 13:36:57 -0500

    // use operator overloading to customize what "+" does

---
+++ C#/teamworkOperatorOverloading.cs
@@ -0,0 +1,43 @@
+// use operator overloading to customize what "+" does
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Score tm1 = new Score(2, 3);
+            Score tm2 = new Score(4, 2);
+
+            Score finalScores = tm1 + tm2;
+
+
+            Console.WriteLine("Round 1: " + finalScores.round1Score);
+            Console.WriteLine("Round 2: " + finalScores.round2Score);
+        }
+    }
+    class Score
+    {
+        public int round1Score { get; set; }
+        public int round2Score { get; set; }
+        public Score(int r1, int r2)
+        {
+            round1Score = r1;
+            round2Score = r2;
+        }
+        
+        //your code goes here
+        public static Score operator +(Score s1, Score s2)
+        {
+            return new Score(s1.round1Score + s2.round1Score, s1.round2Score + s2.round2Score);
+        }
+    }
+}
