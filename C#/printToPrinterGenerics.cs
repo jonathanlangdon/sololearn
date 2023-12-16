22979b865a7e0e37929a788c87512b4aa8a14cca
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-12-16 18:27:09 -0500

    // use a generic to allow any input type

---
+++ C#/printToPrinterGenerics.cs
@@ -0,0 +1,34 @@
+// use a generic to allow any input type
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int intNum = Convert.ToInt32(Console.ReadLine());
+            double doubNum = Convert.ToDouble(Console.ReadLine());
+
+
+            Printer.Print(text);
+            Printer.Print(intNum);
+            Printer.Print(doubNum);
+        }
+    }
+    class Printer
+    {
+        //your code goes here
+        public static void Print<T>(T input)
+        {
+            Console.WriteLine(string.Format($"Showing {input}"));
+        }
+    }
+}
