515d4f7d0bb63bc76f85dd418afcfc322de4edf5
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-12-16 18:27:03 -0500

    // get numbers as input and export as a list

---
+++ C#/TopOfTheLeaderboard.cs
@@ -0,0 +1,32 @@
+// get numbers as input and export as a list
+
+using System;
+using System.Collections.Generic;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfPlayers = Convert.ToInt32(Console.ReadLine());
+
+            List<int> scores = new List<int>();
+            int count = 0;
+            while (count<numOfPlayers)
+            {
+                int score = Convert.ToInt32(Console.ReadLine());
+                //your code goes here
+                scores.Add(score);
+                count += 1;
+            }
+
+            //sort the list and output elements
+            scores.Sort();
+            foreach (int score in scores)
+            {
+                Console.Write(score + " ");
+            }
+        }
+    }
+}
